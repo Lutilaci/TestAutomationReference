7119dab6fe3250ca1118df85ce14c1e38502ca02
Lutilaci <lutilaci@gmail.com>
2022-09-15 14:34:18 +0200

    change WebDriverManager

--- src/main/java/com/codecool/testautomation/config/DriverSingleton.java
+++ src/main/java/com/codecool/testautomation/config/DriverSingleton.java
@@ -5,11 +5,11 @@ import io.github.bonigarcia.wdm.WebDriverManager;
 import org.openqa.selenium.Platform;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.remote.DesiredCapabilities;
+import org.openqa.selenium.remote.RemoteWebDriver;
+
 import java.net.MalformedURLException;
 import java.net.URL;
 import java.time.Duration;
-
-import org.openqa.selenium.remote.RemoteWebDriver;
 
 
 public class DriverSingleton {
