4c9688f98a51bd4b57135d7cbfa082a6d26c021b
Lutilaci <lutilaci@gmail.com>
2022-09-15 14:43:32 +0200

    refactor login method

--- src/main/java/com/codecool/testautomation/page/BasePage.java
+++ src/main/java/com/codecool/testautomation/page/BasePage.java
@@ -36,7 +36,7 @@ public abstract class BasePage {
 
     public void login(){
         openUrl( "login.jsp");
-        waitForWebElementToBePresent(userNameField);
+//        waitForWebElementToBePresent(userNameField);
 
         userNameField.sendKeys(USER_NAME);
         passwordField.sendKeys(PASSWORD);
